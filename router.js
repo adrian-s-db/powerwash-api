a6b430771728d7fb310af26087eb3029d2a19378
Adrian Suarez del Busto <adrian.s.delbusto@gmail.com>
2022-03-08 19:03:50 +0100

    fix: create machine only if not exists

--- router.js
+++ router.js
@@ -1,6 +1,7 @@
 const {Router} = require ('express');
-const { getUser, createUser, updateUser, getAllUsers, deleteAllUsers } = require('./controllers/userControllers');
+const { getUser, createUser, getAllUsers, deleteAllUsers } = require('./controllers/userControllers');
 const { getMachine, createMachine, getAllMachines, getTopMachines, deleteAllMachines } = require('./controllers/machineControllers');
+const { userSaveMachine } = require('../powerwash/services/UsersDbService');
 
 
 const router = Router();
@@ -8,7 +9,7 @@ const router = Router();
 router.post('/users', createUser);
 router.get('/users/:uid', getUser);
 // TODO: Update users
-router.put('/users/:uid', updateUser);
+router.put('/users/save/:uid', userSaveMachine);
 
 router.post('/machines', createMachine);
 router.get('/machines/:id', getMachine);
