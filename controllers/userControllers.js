18a7ae74cd2c5f0f409a024df845165c3720886e
Adrian Suarez del Busto <adrian.s.delbusto@gmail.com>
2022-03-08 17:43:10 +0100

    feat: updating put

--- controllers/userControllers.js
+++ controllers/userControllers.js
@@ -31,9 +31,9 @@ const updateUser = async (req, res) => {
         req.body.user.savedMachines.concat(req.body.machine.washingMachineCode);
 
     console.log('SEARCHING USER WITH ID: ', req.body.user.id)
-    const user = User.findById(req.body.user.id);
+    // const user = User.findById(req.body.user.id);
 
-    console.log(user);
+    // console.log(user);
     // user.savedMachines = updatedMachinesArr;
     // const updatedUser = await user.save();
 
